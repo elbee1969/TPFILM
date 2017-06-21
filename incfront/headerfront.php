0b7dd0aace0ef071fedceb1252271b71c250efa7
BaptisteCG <baptiste.cresteyg@gmail.com>
2017-06-21 12:14:53 +0200

    add

--- incfront/headerfront.php
+++ incfront/headerfront.php
@@ -11,8 +11,28 @@
   <body>
 
     <header>
-
+      <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">TP FILM</a>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+          </ul>
+          <span class="navbar-text">
+            <i class="fa fa-user fa-2x" aria-hidden="true"></i>
+          </span>
+        </div>
+      </nav>
     </header>
 
     <div class="wrapper">
-      
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 include('inc/pdo.php');
 include ('inc/functions.php');
-
 include('incfront/headerfront.php');
-
 include('incfront/footerfront.php');
