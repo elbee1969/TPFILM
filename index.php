15fb37c47a204d06d920cc4ae6b98bd20e723b85
laurent berthelot <laurent.berthelot1969@gmail.com>
2017-06-21 14:01:33 +0200

    page index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include ('inc/functions.php');
 
 include('incfront/headerfront.php');
 ?>
-<p>index</p>
+<h1>Liste des films</h1>
 
 
 
@@ -16,27 +16,27 @@ $sql = "SELECT count(*) FROM `movies_full` ";
 $query = $pdo->prepare($sql);
 $query->execute();
 $count = $query->fetchColumn();
-debug($count);
+//debug($count);
 // on récupère le premier id
 $sql = "SELECT MIN(id) FROM `movies_full` ";
 $query = $pdo->prepare($sql);
 $query->execute();
 $first = $query->fetchColumn();
-debug($first);
+//debug($first);
 //on récupère le dernier id
 $sql = "SELECT MAX(id) FROM `movies_full` ";
 $query = $pdo->prepare($sql);
 $query->execute();
 $last = $query->fetchColumn();
-debug($last);
-
+//debug($last);
+// selection des film dans la BDD ici limité a 30 films ....
 $sql = "SELECT * FROM movies_full WHERE 1 ORDER BY RAND()  LIMIT 30";
 $query = $pdo->prepare($sql);
 $query->execute();
 $movies = $query->fetchAll();
-
+//
 foreach ($movies as $movie) {
-    echo '<div class="col">';
+    echo '<div class="">';
     if (file_exists('brief/posters/'.$movie['id'].'.jpg')) {
       echo '<a href="detail.php?id='.$movie['id'].'"><img src="brief/posters/'.$movie['id'].'.jpg" alt="'.$movie['title'].'"></a>';
     } else {
