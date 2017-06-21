09cf5089bd1acb1f414580207ff666fed34e7b71
laurent berthelot <laurent.berthelot1969@gmail.com>
2017-06-21 16:41:00 +0200

    filtres

--- index.php
+++ index.php
@@ -28,6 +28,22 @@ $query = $pdo->prepare($sql);
 $query->execute();
 $last = $query->fetchColumn();
 //debug($last);
+
+//recuperation des dates de production (la première et la dernière)
+// on récupère la premiere date
+$sql = "SELECT MIN(year) FROM `movies_full` ";
+$query = $pdo->prepare($sql);
+$query->execute();
+$firstDate = $query->fetchColumn();
+debug($firstDate);
+//on récupère la derniere date
+$sql = "SELECT MAX(year) FROM `movies_full` ";
+$query = $pdo->prepare($sql);
+$query->execute();
+$lastDate = $query->fetchColumn();
+debug($lastDate);
+
+
 // selection des film dans la BDD ici limité a 30 films ....
 $sql = "SELECT * FROM movies_full WHERE 1 ORDER BY RAND()  LIMIT 36";
 $query = $pdo->prepare($sql);
@@ -47,7 +63,51 @@ foreach ($movies as $movie) {
     echo '<p>'.$movie['title'].'<p/>';
     echo'</div>';
 }
+
+$liste = array(
+    'info'        => 'Informations',
+    'commande'    => 'Commande',
+    'inscription' => 'inscription',
+      'act '=> 'Action',
+      'av' => 'Adventure',
+      'bio' => 'Biography',
+      'com' => 'Comedy',
+      'cri' => 'Crime',
+      'dra' => 'Drama',
+      'noir' => 'Film-Noir',
+      'fanta' => 'Fantasy',
+      'hor' => 'Horror',
+      'myst' => 'Mystery',
+      'mcal' => 'Musical',
+      'mus' => 'Music',
+      'rom' => 'Romance',
+      'sf' => 'Sci-Fi',
+      'spo' => 'Sport',
+      'thri' => 'Thriller',
+      'war' => 'War',
+      'west' => 'Western'
+);
 ?>
+<form class="" action="index.php" method="post">
+
+    <label for="genres">Catégorie</label>
+    <select class="" name="genres">
+      <option value="none">???</option>
+      <?php
+        foreach ($liste as $key => $value) {?>
+        <option value="<?php echo $key; ?>"<?php if(!empty($_POST['genres'])) { if($_POST['genres'] == $key) { echo ' selected="selected"'; } } ?>><?php echo $value; ?></option>
+        <?php }?>
+    </select>
+
+    <label for="year"></label>
+    <select class="" name="year">
+      <option value="none">???</option>
+<?php
+
+ ?>
+    </select>
+    <input type="submit" name="btnSubmit" value="Filtrer">
+  </form>
 </div>
 
 <a href="index.php">Autre film</a>
