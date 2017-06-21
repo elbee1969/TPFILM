a19d9544564ee8247936a8255a1f4cc652d4b95f
BaptisteCG <baptiste.cresteyg@gmail.com>
2017-06-21 14:19:59 +0200

    ajout fonts

--- incfront/headerfront.php
+++ incfront/headerfront.php
@@ -6,11 +6,15 @@
     <link rel="stylesheet" href="./assetfront/cssfront/normalize.css">
     <link rel="stylesheet" href="./assetfront/cssfront/bootstrap.css">
     <link rel="stylesheet" href="./assetfront/cssfront/font-awesome.css">
+    <link rel="stylesheet" href="./assetfront/cssfront/font-awesome.min.css">
     <link  rel="stylesheet" href="./assetfront/cssfront/stylesfront.css">
   </head>
   <body>
 
     <header>
+<div class="header_item">
+
+</div>
       <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -19,20 +23,18 @@
         <div class="collapse navbar-collapse" id="navbarText">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">ACCUEIL <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
+              <a class="nav-link" href="#">SALUT LES GARS</a>
             </li>
           </ul>
           <span class="navbar-text">
-            <i class="fa fa-user fa-2x" aria-hidden="true"></i>
+            <a href="#"><i class="fa fa-user fa-2x" aria-hidden="true"></i> <a href="#"></a>
           </span>
         </div>
       </nav>
     </header>
 
     <div class="wrapper">
+</div>
