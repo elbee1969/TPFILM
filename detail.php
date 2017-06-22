7422b75a3e67819085d34629aa8183330132c767
BaptisteCG <baptiste.cresteyg@gmail.com>
2017-06-22 09:54:26 +0200

    newimage

--- detail.php
+++ detail.php
@@ -32,33 +32,41 @@ include('incfront/headerfront.php');
 <div class="row">
   <div class="col-3">
 
-
-
-
 <?php foreach ($movies as $movie) {
   if($movie['slug'] == $slug) {
     ?>
-
       <h2><center><?php echo $movie['title']; ?></center></h2>
-      <?php echo '<div><center><img class="imgdetail" src="brief/posters/'. $movie['id'] . '.jpg"' ?> alt=""></center> </div>
+<?php
+      if (file_exists('brief/posters/'.$movie['id'].'.jpg')) {
+        echo '<div><center><img class="imgdetail" src="brief/posters/'.$movie['id'].'.jpg" alt="'.$movie['title'].'">'; ?> </center> </div> <?php
+      } else {
+        echo '<div><center><img class="imgdetail" src="brief/posters/noimage.jpg" alt="'.$movie['title'].'">'; ?> </center> </div> <?php
+      }
+?>
 
  </div>
 
 <div class="col-9">
-
-      <span class="titledetail">Année du film:</span><span> <?php echo $movie['year']; ?></span>
-<br>
+      <span class="titledetail centredetail">Année du film:</span><span> <?php echo $movie['year']; ?></span>
+<br><br>
       <span class="titledetail">Catégorie:</span> <span> <?php echo $movie['genres']; ?></span>
-    <br>  
-      <span class="titledetail">Résumé:</span> <div> <?php echo $movie['plot']; ?></div>
-      <div class="titledetail">Réalisateur:</div> <div> <?php echo $movie['directors']; ?></div>
-      <div class="titledetail">Acteurs:</div> <div> <?php echo $movie['cast']; ?></div>
-      <div class="titledetail">Scénaristes:</div> <div> <?php echo $movie['writers']; ?></div>
-      <div class="titledetail">Durée du film:</div> <div> <?php echo $movie['runtime']; ?></div>
-      <div class="titledetail">Classification:</div> <div> <?php echo $movie['mpaa']; ?></div>
-      <div class="titledetail">Evaluation:</div> <div> <?php echo $movie['rating']; ?></div>
-      <div class="titledetail">Popularité:</div> <div> <?php echo $movie['popularity']; ?></div>
- </div></div>   </div><?php
+      <br><br>
+      <span class="titledetail">Résumé:</span> <span> <?php echo $movie['plot']; ?></span>
+      <br><br>
+      <span class="titledetail">Réalisateur:</span> <span> <?php echo $movie['directors']; ?></span>
+      <br><br>
+      <span class="titledetail">Acteurs:</span> <span> <?php echo $movie['cast']; ?></span>
+      <br><br>
+      <span class="titledetail">Scénaristes:</span> <span> <?php echo $movie['writers']; ?></span>
+      <br><br>
+      <span class="titledetail">Durée du film:</span> <span> <?php echo $movie['runtime']; ?></span>
+      <br><br>
+      <span class="titledetail">Classification:</span> <span> <?php echo $movie['mpaa']; ?></span>
+      <br><br>
+      <span class="titledetail">Evaluation:</span> <span> <?php echo $movie['rating']; ?></span>
+      <br><br>
+      <span class="titledetail">Popularité:</span> <span> <?php echo $movie['popularity']; ?></span>
+ </div></div></div><?php
   }
  }
 
